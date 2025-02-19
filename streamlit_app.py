d3640f7ba31aac07b2ee6d2c8a5385d3998a12e4
Dipal Mahajan <Dipal.Mahajan@snowflake.com>
2025-02-19 21:40:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from snowflake.snowpark.functions import col
 import requests
 
 # Write directly to the app
-st.title("Customize Your Avocado Sandwich --> ")
+st.title("Customize Your Smoothie --> ")
 st.write(
     """
     Choose the fruits you want in your custom Smoothie!

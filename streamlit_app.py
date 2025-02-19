61b15444bde87714d60ba6d28a9d8cfff74367c4
Dipal Mahajan <Dipal.Mahajan@snowflake.com>
2025-02-19 21:22:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,8 @@ if ingredient_list:
 
     for fruit_chosen in ingredient_list:
         ingredient_string += fruit_chosen
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json() , use_container_width = True)
 
     #st.write(ingredient_string)
 
@@ -52,9 +54,7 @@ if ingredient_list:
         st.success('Your Smoothie is ordered:' +name_on_order, icon="✅")
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json() , use_container_width = True)
+
 
     
 
